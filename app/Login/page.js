527a3ce9566fc9aafbcd2fd012bd2c1a4a8cc10e
Zurriyatt <hybridkatsszu@gmail.com>
2026-06-27 23:31:01 +0500

    Update page.js

--- app/Login/page.js
+++ app/Login/page.js
@@ -137,7 +137,7 @@ sessionStorage.setItem("toast", JSON.stringify(toastLogin));
               onClick={handleOneClick}
               name="github"
               type="button"
-              className="text-white hover:scale-[1.02] w-[80%] pl-5 bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2 font-sans"
+              className="cursor-pointer text-white hover:scale-[1.02] w-[80%] pl-5 bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2 font-sans"
             >
               <svg
                 className="w-4 h-4 me-2 pointer-events-none"
@@ -159,7 +159,7 @@ sessionStorage.setItem("toast", JSON.stringify(toastLogin));
               onClick={handleOneClick}
               name="google"
               type="button"
-              className="text-white hover:scale-[1.02] w-[80%] pl-5 bg-[#4285F4] hover:bg-[#4285F4]/70 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2 font-sans"
+              className="cursor-pointer text-white hover:scale-[1.02] w-[80%] pl-5 bg-[#4285F4] hover:bg-[#4285F4]/70 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2 font-sans"
             >
               <svg
                 className="w-4 h-4 me-2 pointer-events-none"
